cba09ee852d19efb4579de503efcf7e91d2d7a43
munjung <jekang0123@gmail.com>
2019-05-08 23:33:42 +0900

    로그인 페이지 snapkit으로 레아아웃 다시함

--- mycat/mycat/controller/LoginViewController.swift
+++ mycat/mycat/controller/LoginViewController.swift
@@ -7,17 +7,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var registerBtn: UIButton!
+
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var registerBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        registerBtn.layer.cornerRadius = 10.0
-        loginBtn.layer.cornerRadius = 10.0
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(logoImage)
+        self.view.addSubview(idTextField)
+        self.view.addSubview(pwTextField)
+        self.view.addSubview(loginBtn)
+        self.view.addSubview(registerBtn)
+        
+        logoImage.snp.makeConstraints { (make) in
+            make.leading.equalTo(self.view).offset(20)
+            make.trailing.equalTo(self.view).offset(-20)
+            make.height.equalTo(200)
+            make.top.equalTo(self.view.snp_topMargin).offset(75)
+        }
+
+        idTextField.snp.makeConstraints { (make) in
+            make.leading.equalTo(self.view).offset(20)
+            make.trailing.equalTo(self.view).offset(-20)
+            make.height.equalTo(45)
+            make.top.equalTo(logoImage.snp_bottomMargin).offset(25)
+        }
+
+        pwTextField.snp.makeConstraints { (make) in
+            make.leading.equalTo(self.view).offset(20)
+            make.trailing.equalTo(self.view).offset(-20)
+            make.height.equalTo(45)
+            make.top.equalTo(idTextField.snp_bottomMargin).offset(25)
+        }
+
+        loginBtn.snp.makeConstraints { (make) in
+            loginBtn.layer.cornerRadius = 10.0
+            make.leading.equalTo(self.view).offset(20)
+            make.trailing.equalTo(self.view).offset(-20)
+            make.height.equalTo(45)
+            make.top.equalTo(pwTextField.snp_bottomMargin).offset(25)
+        }
+        
+        registerBtn.snp.makeConstraints { (make) in
+            registerBtn.layer.cornerRadius = 10.0
+            make.leading.equalTo(self.view).offset(20)
+            make.trailing.equalTo(self.view).offset(-20)
+            make.height.equalTo(45)
+            make.bottom.equalTo(self.view.snp_bottomMargin).offset(-75)
+        }
+        
     }
     
 
